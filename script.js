c09fc09ce9fc7b9afeee89a366ed9fd742f6f9e3
lowlyphe <jones.kyle2893@gmail.com>
2022-06-23 17:16:42 -0500

    completed random color

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
 // Your JS goes here
 
-function createBoard(num) {
-  for (let i = 0; i < num / 2 - 1; i++) {
+for (let i = 0; i < 8; i++) {
+  for (let j = 0; j < 8; j++) {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    console.log(randomColor);
     let div = document.createElement("div");
-    div.classList = "black";
-    div.style.color = "black";
-    div.style.width = "11.1%";
-    div.style.paddingBottom = "11.1%";
-    div.style.backgroundColor = "black";
-    div.style.float = "left";
-    document.body.append(div);
-    div = document.createElement("div");
-    div.classList = "red";
-    div.style.color = "red";
-    div.style.width = "11.1%";
-    div.style.paddingBottom = "11.1%";
-    div.style.backgroundColor = "red";
+    div.style.width = "12%";
+    div.style.paddingBottom = "12%";
+    div.style.backgroundColor = "#" + randomColor;
     div.style.float = "left";
     document.body.append(div);
   }
-  let div = document.createElement("div");
-  div.classList = "black";
-  div.style.color = "black";
-  div.style.width = "11.1%";
-  div.style.paddingBottom = "11.1%";
-  div.style.backgroundColor = "black";
-  div.style.float = "left";
-  document.body.append(div);
 }
-
-createBoard(64);
