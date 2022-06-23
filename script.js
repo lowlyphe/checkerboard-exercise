9c22b36ce58aeaa80986695a3fab9f10a8774233
lowlyphe <jones.kyle2893@gmail.com>
2022-06-23 17:10:38 -0500

    fixed it

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 // Your JS goes here
-
-function createBoard(num) {
-  for (let i = 0; i < num / 2 - 1; i++) {
-    let div = document.createElement("div");
-    div.classList = "black";
-    div.style.color = "black";
-    div.style.width = "11.1%";
-    div.style.paddingBottom = "11.1%";
-    div.style.backgroundColor = "black";
-    div.style.float = "left";
-    document.body.append(div);
-    div = document.createElement("div");
-    div.classList = "red";
-    div.style.color = "red";
-    div.style.width = "11.1%";
-    div.style.paddingBottom = "11.1%";
-    div.style.backgroundColor = "red";
-    div.style.float = "left";
-    document.body.append(div);
+var row = 1;
+for (let i = 0; i < 8; i++) {
+  for (let j = 0; j < 8; j++) {
+    if (row % 2 === 0) {
+      if (j % 2 === 0) {
+        styleBLack();
+      } else {
+        styleRed();
+      }
+    } else {
+      if (j % 2 === 0) {
+        styleRed();
+      } else {
+        styleBLack();
+      }
+    }
   }
+  row++;
+}
+
+function styleBLack() {
   let div = document.createElement("div");
-  div.classList = "black";
-  div.style.color = "black";
-  div.style.width = "11.1%";
-  div.style.paddingBottom = "11.1%";
+  div.style.width = "12%";
+  div.style.paddingBottom = "12%";
   div.style.backgroundColor = "black";
   div.style.float = "left";
   document.body.append(div);
 }
-
-createBoard(64);
+function styleRed() {
+  let div = document.createElement("div");
+  div.style.width = "12%";
+  div.style.paddingBottom = "12%";
+  div.style.backgroundColor = "red";
+  div.style.float = "left";
+  document.body.append(div);
+}
