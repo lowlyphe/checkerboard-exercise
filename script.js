538ddf03ead26ae63025582f26b94381a63c0e25
lowlyphe <jones.kyle2893@gmail.com>
2022-06-23 15:13:59 -0500

    completed checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // Your JS goes here
+
+function createBoard(num) {
+  for (let i = 0; i < num / 2 - 1; i++) {
+    let div = document.createElement("div");
+    div.classList = "black";
+    div.style.color = "black";
+    div.style.width = "11.1%";
+    div.style.paddingBottom = "11.1%";
+    div.style.backgroundColor = "black";
+    div.style.float = "left";
+    document.body.append(div);
+    div = document.createElement("div");
+    div.classList = "red";
+    div.style.color = "red";
+    div.style.width = "11.1%";
+    div.style.paddingBottom = "11.1%";
+    div.style.backgroundColor = "red";
+    div.style.float = "left";
+    document.body.append(div);
+  }
+  let div = document.createElement("div");
+  div.classList = "black";
+  div.style.color = "black";
+  div.style.width = "11.1%";
+  div.style.paddingBottom = "11.1%";
+  div.style.backgroundColor = "black";
+  div.style.float = "left";
+  document.body.append(div);
+}
+
+createBoard(64);
